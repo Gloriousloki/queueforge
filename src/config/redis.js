384f7237325766ed3d9e2468a94c6fe1a03a1be9
Bhuvan <bhuvi0058@gmail.com>
2026-07-27 21:48:34 +0530

    Add Redis password for Upstash authentication

--- src/config/redis.js
+++ src/config/redis.js
@@ -9,6 +9,7 @@ function getRedis() {
         redis = new IORedis({
             host: env.REDIS_HOST,
             port: env.REDIS_PORT,
+            password: env.REDIS_PASSWORD,
             tls: {}, 
             maxRetriesPerRequest: null,
         });
